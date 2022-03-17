ab9470f0c32d5b53ceb5d15705c56b4f2da7d5f8
yanpengju <yanpengju@masastack.com>
2022-03-17 18:21:00 +0800

    feat(index): add index page

--- src/UI/MASA.PM.UI.Admin/Pages/Home/Index.razor.cs
+++ src/UI/MASA.PM.UI.Admin/Pages/Home/Index.razor.cs
@@ -1,7 +1,26 @@
-﻿namespace MASA.PM.UI.Admin.Pages.Home
+﻿using MASA.PM.Caller.Callers;
+
+namespace MASA.PM.UI.Admin.Pages.Home
 {
     public partial class Index
     {
+        [Inject]
+        public EnvironmentCaller EnvironmentCaller { get; set; } = default!;
 
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var envs = await EnvironmentCaller.GetListAsync();
+                if (envs.Count > 0)
+                {
+                    Nav.NavigateTo(GlobalVariables.DefaultRoute, true);
+                }
+                else
+                {
+                    Nav.NavigateTo("init", true);
+                }
+            }
+        }
     }
 }
