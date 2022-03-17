e0c5f672cd46de94d034782c16ef31966b451dc3
yanpengju <yanpengju@masastack.com>
2022-03-17 17:09:41 +0800

    feat(api): Modify project structure and add api service

---
+++ src/Services/MASA.PM.Service.Admin/Migrations/20220316064740_update_project_table.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MASA.PM.Service.Admin.Migrations
+{
+    public partial class update_project_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Type",
+                table: "Projects",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                comment: "Type");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Projects");
+        }
+    }
+}
