8b99d153237b6dfe8cf2ac02133d8d738ee0ebc8
yanpengju <yanpengju@masastack.com>
2022-04-19 16:30:46 +0800

    style: update app model

--- src/Web/MASA.PM.Web.Admin/Pages/Home/Team.razor.cs
+++ src/Web/MASA.PM.Web.Admin/Pages/Home/Team.razor.cs
@@ -204,7 +204,7 @@ namespace MASA.PM.Web.Admin.Pages.Home
                 Description = _appDetail.Description,
                 SwaggerUrl = _appDetail.SwaggerUrl,
                 Url = _appDetail.Url,
-                EnvironmentClusterIds = _appDetail.EnvironmentClusters.Select(envCluster => envCluster.EnvironmentCluster.Id).ToList()
+                EnvironmentClusterIds = _appDetail.EnvironmentClusters.Select(envCluster => envCluster.Id).ToList()
             });
         }
 
