e3135f7c5eaf838f133fdf450ed379a17abc44e8
pengjuyan <42963214+codding-y@users.noreply.github.com>
2022-08-03 17:02:34 +0800

    fix: fix update project id bug (#168)
    * fix: fix update project id bug
    
    * style: update tabs css
    
    Co-authored-by: yanpengju <yanpengju@masastack.com>

--- src/Contracts/MASA.PM.Contracts.Admin/Dto/AddProjectDto.cs
+++ src/Contracts/MASA.PM.Contracts.Admin/Dto/AddProjectDto.cs
@@ -16,7 +16,7 @@ namespace MASA.PM.Contracts.Admin.Dto
             set => _name = value?.Trim() ?? "";
         }
 
-        [RegularExpression(@"^[Za-z0-9_-]+$", ErrorMessage = "Please enter [Chinese, English、and - _ symbols] ")]
+        [RegularExpression(@"^[a-zA-Z0-9_-]+$", ErrorMessage = "Please enter [Chinese, English、and - _ symbols] ")]
         [Required(ErrorMessage = "Project identity is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "App identity length range is [2-50]")]
         public string Identity { get; set; } = "";
