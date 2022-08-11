7cc42cbdb41f82a5e69c006c9f98e8dbb176e92c
pengjuyan <42963214+codding-y@users.noreply.github.com>
2022-08-11 14:22:01 +0800

    style: update style (#171)
    Co-authored-by: yanpengju <yanpengju@masastack.com>

--- src/Services/MASA.PM.Service.Admin/Infrastructure/PMDbContext.cs
+++ src/Services/MASA.PM.Service.Admin/Infrastructure/PMDbContext.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.Data.EntityFrameworkCore;
-
 namespace MASA.PM.Service.Admin.Infrastructure
 {
     public class PmDbContext : MasaDbContext
