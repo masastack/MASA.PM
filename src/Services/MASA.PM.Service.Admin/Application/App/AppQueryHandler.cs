0992f8b923adeeb95afee8528aad6a33542af077
pengjuyan <42963214+codding-y@users.noreply.github.com>
2022-11-01 18:05:21 +0800

    feat: update app list api (#221)
    Co-authored-by: yanpengju <yanpengju@masastack.com>

--- src/Services/MASA.PM.Service.Admin/Application/App/AppQueryHandler.cs
+++ src/Services/MASA.PM.Service.Admin/Application/App/AppQueryHandler.cs
@@ -74,18 +74,89 @@ namespace MASA.PM.Service.Admin.Application.Cluster
         {
             if (query.ProjectIds.Any())
             {
-                var apps = await _appRepository.GetListByProjectIdAsync(query.ProjectIds);
+                List<Infrastructure.Entities.App> apps = await _appRepository.GetListByProjectIdAsync(query.ProjectIds);
+                List<(EnvironmentClusterProjectApp EnvironmentClusterProjectApp,
+                     int ProjectId,
+                     string ClusterName,
+                     string EnvName,
+                     string EnvColor,
+                     EnvironmentCluster EnvCluster)> appProjectEnvClusters = await _appRepository.GetEnvironmentAndClusterNamesByAppIds(apps.Select(app => app.Id));
+                var appEnvironmentClusters = appProjectEnvClusters
+                      .Where(appProjectEnvCluster => query.ProjectIds.Contains(appProjectEnvCluster.ProjectId))
+                      .Select(appProjectEnvCluster => new AppEnvironmentClusterDto
+                      {
+                          AppId = appProjectEnvCluster.EnvironmentClusterProjectApp.AppId,
+                          ProjectId = appProjectEnvCluster.ProjectId,
+                          EnvironmentCluster = new EnvironmentClusterDto
+                          {
+                              Id = appProjectEnvCluster.EnvCluster.Id,
+                              EnvironmentName = appProjectEnvCluster.EnvName,
+                              EnvironmentColor = appProjectEnvCluster.EnvColor,
+                              ClusterName = appProjectEnvCluster.ClusterName,
+                              AppURL = appProjectEnvCluster.EnvironmentClusterProjectApp.AppURL,
+                              AppSwaggerURL = appProjectEnvCluster.EnvironmentClusterProjectApp.AppSwaggerURL
+                          }
+                      }).ToList();
+
+                var environmentClusterGroup = appEnvironmentClusters.GroupBy(c => new { c.ProjectId, c.AppId }).ToList();
+
+                var result = apps.Join(
+                        environmentClusterGroup,
+                        app => app.Id,
+                        environmentCluster => environmentCluster.Key.AppId,
+                        (app, environmentClusters) => new { app, environmentClusters });
+
+                query.Result = result.Select(appEnvironmentCluster => new AppDto
+                {
+                    ProjectId = appEnvironmentCluster.environmentClusters.Key.ProjectId,
+                    Name = appEnvironmentCluster.app.Name,
+                    Description = appEnvironmentCluster.app.Description,
+                    Id = appEnvironmentCluster.app.Id,
+                    Identity = appEnvironmentCluster.app.Identity,
+                    Type = appEnvironmentCluster.app.Type,
+                    ServiceType = appEnvironmentCluster.app.ServiceType,
+                    Creator = appEnvironmentCluster.app.Creator,
+                    CreationTime = appEnvironmentCluster.app.CreationTime,
+                    ModificationTime = appEnvironmentCluster.app.ModificationTime,
+                    Modifier = appEnvironmentCluster.app.Modifier,
+                    EnvironmentClusters = appEnvironmentCluster.environmentClusters.Select(envCluster => new EnvironmentClusterDto
+                    {
+                        Id = envCluster.EnvironmentCluster.Id,
+                        EnvironmentName = envCluster.EnvironmentCluster.EnvironmentName,
+                        EnvironmentColor = envCluster.EnvironmentCluster.EnvironmentColor,
+                        ClusterName = envCluster.EnvironmentCluster.ClusterName,
+                        AppURL = envCluster.EnvironmentCluster.AppURL,
+                        AppSwaggerURL = envCluster.EnvironmentCluster.AppSwaggerURL
+                    }).ToList()
+                }).OrderByDescending(app => app.ModificationTime)
+                .ToList();
+            }
+            else
+            {
+                List<Infrastructure.Entities.App> apps = await _appRepository.GetListAsync();
+                query.Result = apps.Select(app => new AppDto
+                {
+                    Name = app.Name,
+                    Description = app.Description,
+                    Id = app.Id,
+                    Identity = app.Identity,
+                    Type = app.Type,
+                    ServiceType = app.ServiceType,
+                    Creator = app.Creator,
+                    CreationTime = app.CreationTime,
+                    ModificationTime = app.ModificationTime,
+                    Modifier = app.Modifier
+                }).OrderByDescending(app => app.ModificationTime)
+                .ToList();
 
                 List<(EnvironmentClusterProjectApp EnvironmentClusterProjectApp,
-                    int ProjectId,
-                    string ClusterName,
-                    string EnvName,
-                    string EnvColor,
-                    EnvironmentCluster EnvCluster)>
-                appProjectEnvClusters = await _appRepository.GetEnvironmentAndClusterNamesByAppIds(apps.Select(app => app.Id));
+                      int ProjectId,
+                      string ClusterName,
+                      string EnvName,
+                      string EnvColor,
+                      EnvironmentCluster EnvCluster)> appProjectEnvClusters = await _appRepository.GetEnvironmentAndClusterNamesByAppIds(apps.Select(app => app.Id));
 
                 var appEnvironmentClusters = appProjectEnvClusters
-                    .Where(appProjectEnvCluster => query.ProjectIds.Contains(appProjectEnvCluster.ProjectId))
                     .Select(appProjectEnvCluster => new AppEnvironmentClusterDto
                     {
                         AppId = appProjectEnvCluster.EnvironmentClusterProjectApp.AppId,
@@ -131,24 +202,6 @@ namespace MASA.PM.Service.Admin.Application.Cluster
                         AppURL = envCluster.EnvironmentCluster.AppURL,
                         AppSwaggerURL = envCluster.EnvironmentCluster.AppSwaggerURL
                     }).ToList()
-                }).OrderByDescending(app => app.ModificationTime)
-                .ToList();
-            }
-            else
-            {
-                var apps = await _appRepository.GetListAsync();
-                query.Result = apps.Select(app => new AppDto
-                {
-                    Name = app.Name,
-                    Description = app.Description,
-                    Id = app.Id,
-                    Identity = app.Identity,
-                    Type = app.Type,
-                    ServiceType = app.ServiceType,
-                    Creator = app.Creator,
-                    CreationTime = app.CreationTime,
-                    ModificationTime = app.ModificationTime,
-                    Modifier = app.Modifier
                 }).OrderByDescending(app => app.ModificationTime)
                 .ToList();
             }
