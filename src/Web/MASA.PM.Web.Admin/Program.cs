cd7afadc3cf11fe610e9a73e2f44faf199dc0d88
Mayue <mayue8373@live.com>
2023-03-29 20:39:57 +0800

    feat:update package

--- src/Web/MASA.PM.Web.Admin/Program.cs
+++ src/Web/MASA.PM.Web.Admin/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+using FluentValidation;
+using FluentValidation.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
 
 ValidatorOptions.Global.LanguageManager = new MasaLanguageManager();
