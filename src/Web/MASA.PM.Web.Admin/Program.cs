ae5d91a9192f2f257e4bfb0f26dc9d17408bf28d
yanpengju <yanpengju@masastack.com>
2023-03-30 15:11:59 +0800

    chore: remove using

--- src/Web/MASA.PM.Web.Admin/Program.cs
+++ src/Web/MASA.PM.Web.Admin/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
-using FluentValidation;
-using FluentValidation.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
 
