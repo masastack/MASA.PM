e5f08cd28c26b13f9c2687e4d5e2ca498e69ed08
Qinyouzeng <102203523+Qinyouzeng@users.noreply.github.com>
2024-10-28 18:06:42 +0800

    fix: fix app update name show exist error (#435)

--- src/Services/MASA.PM.Service.Admin/Infrastructure/Repositories/AppRepository.cs
+++ src/Services/MASA.PM.Service.Admin/Infrastructure/Repositories/AppRepository.cs
@@ -165,7 +165,7 @@ namespace MASA.PM.Service.Admin.Infrastructure.Repositories
                                 join env in _dbContext.Environments on envCluster.EnvironmentId equals env.Id
                                 join cluster in _dbContext.Clusters on envCluster.ClusterId equals cluster.Id
                                 join ecpa in _dbContext.EnvironmentClusterProjectApps on ecp.Id equals ecpa.EnvironmentClusterProjectId
-                                join app in _dbContext.Apps.Where(app => app.Name == name || app.Identity.ToLower() == identity && !excludeAppIds.Contains(app.Id)) on ecpa.AppId equals app.Id
+                                join app in _dbContext.Apps.Where(app => (app.Name == name || app.Identity.ToLower() == identity) && !excludeAppIds.Contains(app.Id)) on ecpa.AppId equals app.Id
                                 select new
                                 {
                                     EnvironmentName = env.Name,
